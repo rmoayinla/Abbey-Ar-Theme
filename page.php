9d36700b138ef2c73b5fdc512bd110dec2c1074c
rmoayinla <rmoayinla@yahoo.com>
2017-05-20 17:38:35 +0100

    added a new hook to run before content and a new hook to run before displaying the template file

--- page.php
+++ page.php
@@ -5,33 +5,44 @@
 * example copy the content here, create a file and name it page-contact.php to display contact page 
 * 
 */
+
 $no_sidebar = ( abbey_custom_field( "no_sidebar" ) == 0 ) ? false  : true;
 $thumbnail_url = $thumbnail_class = "";
- if ( has_post_thumbnail() ) {
+if ( has_post_thumbnail() ) {
  	$thumbnail_url = get_the_post_thumbnail_url();
  	$thumbnail_class = "has-thumbnail"; 
 }
+
 $page_background = !empty( abbey_custom_field( "page_background" ) ) ? abbey_custom_field( "page_background" ) : "";
 
 ?>
 
 <?php get_header(); ?>
-<main id="<?php abbey_theme_page_id(); ?>" class="row site-content <?php if( $no_sidebar ){ echo "no-sidebar";}?>"
-	style="backgound-image:url(<?php echo $page_background; ?>)"> 
-	<?php if ( have_posts() ) : ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php global $more; $more = 0; ?>
 
-			<header id="page-content-header" class="<?php echo esc_attr( $thumbnail_class ); ?>">
-				<div id="page-title">
-					<div class="page-title-wrap">
-						<h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
-						<h4><?php abbey_custom_field( "sub_title", true ); ?> </h4>
-						<summary class="description" itemprop="summary"><em><?php the_excerpt(); ?></em></summary>
-					</div>
+	<main id="<?php abbey_theme_page_id(); ?>" 
+		class="row site-content <?php if( $no_sidebar ){ echo "no-sidebar";}?>"
+	> 
+	
+	<?php if ( have_posts() ) : ?>
+	
+		<?php while ( have_posts() ) : the_post(); ?>
+		
+			<?php global $more; $more = 0; ?>
+			<header id="site-content-header" class="before-content"><?php do_action( "abbey_theme_before_content" ); ?></header>
+			
+			<header id="page-content-header">
+				<div class="page-title-wrap md-50">		
+					<h1 class="page-title no-top-margin" itemprop="headline"><?php the_title(); ?></h1>
+					
+					<h4><?php abbey_custom_field( "sub_title", true ); ?> </h4>
+					
+					<summary class="description" itemprop="summary"><?php the_excerpt(); ?></summary>
 				</div>
+
 			</header><!-- #page-content-header closes -->
+
 			<div class="row inner-content" style="background-image:url(<?php abbey_custom_field( "page_background", true );?> );">
+				
 				<?php get_template_part("templates/content", "page"); ?>
 				
 				<?php if ( !$no_sidebar ) : ?>
@@ -39,7 +50,9 @@ $page_background = !empty( abbey_custom_field( "page_background" ) ) ? abbey_cus
 						<?php abbey_display_sidebar( "sidebar-main" ); ?>
 					</aside>
 				<?php endif; ?>
+
 			</div>
+			
 		<?php endwhile; ?> 
 
 	<?php else : get_template_part("templates/content", "none");?>
@@ -48,5 +61,5 @@ $page_background = !empty( abbey_custom_field( "page_background" ) ) ? abbey_cus
 
 
 </main> <!--main #page closes -->	<?php 
-	global $authordata;  print_r( $authordata );
+	
 get_footer();
