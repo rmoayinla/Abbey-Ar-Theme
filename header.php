3d0b9e580acbc5bd844c1131ec45fe75f830b5ae
rmoayinla <rmoayinla@yahoo.com>
2017-05-26 23:54:59 +0100

    made my header.php run the same hook that is in custom-header.php for adding meta tags

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<?php do_action( "abbey_theme_header_metas" ); ?>
+		<?php do_action( "abbey_theme_header_meta_tags" ); ?>
 
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
