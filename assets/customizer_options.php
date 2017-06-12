f27ba4a2a4002f0ce5935f0e5cd8d2161f2f45ed
rmoayinla <rmoayinla@yahoo.com>
2017-06-12 09:25:48 +0100

    started adding customizer settings to the theme customizer, the settings are generated by Kirki customizer plugin, my theme settings class Abbey_Theme_Settings handles the theme default options

--- assets/customizer_options.php
+++ assets/customizer_options.php
@@ -1,6 +1,43 @@
 <?php 
 function customizer_settings_init(){
+	if( !class_exists( "Kirki" ) || !class_exists( "Abbey_theme_settings" ) )
+		return; 
 	
+	
+	Abbey_theme_settings::init(); 
+	
+
+	
+	$customizer_config = array( 
+		"option_type"	=>	"option", 
+		"option_name"	=>	"abbey_theme" 
+	); 
+
+	$config_id = "abbey_theme";
+
+	Kirki::add_config( $config_id, $customizer_config );
+
+	Abbey_theme_settings::$option_key = $config_id; 
+
+	$section = $panel = "";
+
+	$section = "colors";
+	Kirki::add_field( $config_id, array(
+			"settings"	=>	"primary_color", 
+			"type"		=>	"color",
+			"label"		=>	esc_html__( "Select theme primary/main colour", "abbey" ),	
+			"section"	=>	$section, 
+			"default"	=>	Abbey_theme_settings::get_default_options( "gray_color", "colors"  ),
+			"transport"	=>	"postMessage", 
+			"js_vars"	=> array(
+					array(
+						"element"	=>	".primary, p, body",
+						"function"	=>	"css", 
+						"property"	=>  "color" 
+					)
+				)
+		) 
+	);
 	
 }
 
