2d27e4e4af2cc0a03c5b8bd163863a3c62dfd679
rmoayinla <rmoayinla@yahoo.com>
2017-07-24 16:11:09 +0100

    A cleaner and more legible comment block, added a new author index key to abbey default settings.

--- functions/theme_setup.php
+++ functions/theme_setup.php
@@ -108,24 +108,24 @@ function abbey_theme_front_page_defaults(){
 	return $abbey_defaults["front-page"];
 }
 
-/*
-* function to get a particular key from global defaults 
-* @return: array or string - depending on the key requested, if key is not found it returns an empty string 
-*
-*
-*/
+/**
+ * Get a particular key from global defaults 
+ * @return: array or string - depending on the key requested, if key is not found it returns an empty string 
+ * @since: 0.1
+ * @notice: this function might be deprecated or removed in future version
+ */
 function abbey_get_defaults( $key ){
 	global $abbey_defaults;
 
 	return ( isset( $abbey_defaults[$key] ) ) ? $abbey_defaults[$key] : "";
 }
 
-/*
-* A filter for Abbey defaults adding list of services to be displayed 
-* @return: array - Abbey defaults 
-*
-*
-*/
+ /**
+ * A filter for Abbey defaults adding list of services to be displayed 
+ * @since: 0.1 
+ * @return: array 	$defaults 	Abbey theme defaults options 
+ *
+ */
 add_filter( "abbey_theme_defaults", "abbey_theme_add_services" );
 
 function abbey_theme_add_services($defaults){
@@ -143,15 +143,15 @@ function abbey_theme_add_services($defaults){
 	return $defaults; // dont forget to return defaults in all filters //
 }
 
-/*
-* A filter to add contacts i.e. email, phone no, address etc
-* @return: array - Abbey defaults 
-*
-*
+/**
+ * A filter to add contacts i.e. email, phone no, address etc
+ * @return: array  	$defaults 		Abbey theme defaults settings/options
+ * @param: array 	$defaults 		abbey theme defaults settings/options
+ * @since: 0.1
 */
 add_filter( "abbey_theme_defaults", "abbey_theme_add_contacts", 30 );
 
-function abbey_theme_add_contacts($defaults){
+function abbey_theme_add_contacts( $defaults ){
 	$contacts = $defaults["contacts"]; // default contacts //
 	
 	$contacts["address"] = array(
@@ -174,13 +174,12 @@ function abbey_theme_add_contacts($defaults){
 	return $defaults; // always return defaults for all filter
 }
 
-/*
-* Filter for adding slide images, carousel, posts for the front page 
-* this filter works only on the frontpage 
-* @return: array - Abbey defaults 
-*
-*
-*/
+/**
+ * A filter for adding slide images, carousel, posts for the front page 
+ * this filter works only on the frontpage 
+ * @return: array - Abbey defaults 
+ *
+ */
 add_filter( "abbey_theme_defaults", "abbey_add_front_page_slides", 40 );//filter to extend front page slides and slide images //
 if( !function_exists( "abbey_add_front_page_slides" ) ) :
 
@@ -188,6 +187,7 @@ if( !function_exists( "abbey_add_front_page_slides" ) ) :
 
 		$front_page = $defaults["front-page"]; // default front-page key containing all front-page defaults //
 		$news =  get_post_type_object("news");
+		
 		/*
 		* Adding a key for slides to front-page defaults
 		* this key is a multi-dimensional array with each array containing info for each slide 
@@ -199,7 +199,16 @@ if( !function_exists( "abbey_add_front_page_slides" ) ) :
 				array(
 					"image" => get_template_directory_uri()."/img/banner-1.jpg"
 				), 	
+				
 				array(
+					"image" => get_template_directory_uri()."/img/banner-3.jpg"
+				 )
+
+			)
+		);
+
+		if( !empty( $news ) ){
+			$front_page[ "slides" ][] = array(
 					"image" => get_template_directory_uri()."/img/banner-2.jpg", 
 					"caption" => array(
 						"title" => $news->labels->archives,
@@ -209,13 +218,9 @@ if( !function_exists( "abbey_add_front_page_slides" ) ) :
 							"post_type" => "news", "posts_per_page" => 2
 						)
 					)
-				 ),
-				array(
-					"image" => get_template_directory_uri()."/img/banner-3.jpg"
-				 )
-
-			)
-		);
+			);
+		}
+		
 		$defaults[ "front-page" ] = wp_parse_args ( $front_page, $defaults["front-page"] ); //override my default front-page with this one //
 		
 		return $defaults; // return the defaults as usual for all theme default filters //
@@ -224,13 +229,13 @@ if( !function_exists( "abbey_add_front_page_slides" ) ) :
 
 endif; //endif function_exist abbey_add_front_page_slides //
 
-/*
-* A filter to add the administrator/owner info to Abbey theme 
-* these infos can be owner's email, photo and contacts 
-* @return: array - Abbey defaults 
-*
-*
-*/
+/**
+ * A filter to add the administrator/owner info to Abbey theme 
+ * these infos can be owner's email, photo and contacts 
+ * @return: array  		$defaults  Abbey defaults with populated admin info 
+ * @param: array 		$defaults 	abbey theme default settings 
+ * @since: 0.1
+ */
 add_filter( "abbey_theme_defaults", "abbey_add_admin_info", 50 );
 
 function abbey_add_admin_info( $defaults ){
@@ -257,6 +262,14 @@ function abbey_add_admin_info( $defaults ){
 	return $defaults; 
 }
 
+/**
+ * A filter to populate theme color settings and info
+ * These color options will be used generally to style pages, posts, links, buttons etc
+ * @since: 0.1
+ * @updated: 
+ * @param: array 		$defaults 		abbey theme default settings 
+ * @return: array 		$defaults 		new default settings with color options 
+ */
 add_filter( "abbey_theme_defaults", "abbey_add_colors", 55 );
 function abbey_add_colors( $defaults ){
 	$colors = array(
@@ -268,7 +281,13 @@ function abbey_add_colors( $defaults ){
 		"gray_color"		=> "#eee"
 	);
 
-	$colors = apply_filters( "abbey_theme_default_colors", $colors );
+	$colors = apply_filters( "abbey_theme_default_colors", $colors ); //filter to edit/change/remove color options //
+	
+	/** 
+	 * Override the default colors with our new color settings
+	 * Note: the override is done after the filter 
+	 * so newer user color options added in filter will override default options too
+	 */
 	$defaults[ "colors" ] = wp_parse_args( $colors, $defaults[ "colors" ] );
 
 	return $defaults;
@@ -282,5 +301,22 @@ function abbey_archive_defaults( $defaults ){
 		"sidebar" => 0,
 		"ajax_load_posts" => 1
 	);
+	return $defaults;
+}
+
+/**
+ * Add authors settings key to the theme default settings 
+ * This author setting will handle authors default photo, social contacts list for authors etc 
+ * @since: 0.11
+ * @param: array 		$defaults 		the theme default settings 
+ * @return: array 		$defaults 		the theme default settings that was passed 
+ */
+add_filter( "abbey_theme_defaults", "abbey_author_defaults", 90 );
+function abbey_author_defaults( $defaults ){
+
+	$defaults[ "authors" ] = array(
+		"social_contacts" => apply_filters( "abbey_authors_social_contacts", array( "facebook", "twitter", "google-plus", "linkedin", "github" ) ), 
+		"default_photo" => "", 
+		);
 	return $defaults;
 }
