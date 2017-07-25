b25b14cc80b3ee421461a7fa39eee460d6ed75ab
rmoayinla <rmoayinla@yahoo.com>
2017-07-25 10:08:55 +0100

    Using a function to generate post count which will be added to each post panel class attribute. Made some changes in the code display and arrangement for readability

--- category.php
+++ category.php
@@ -2,10 +2,10 @@
 get_header();
 
 global $count, $wp_query, $abbey_query, $abbey_defaults;
+
 $current_page = $queried_object = $queried_name = $archive_options = "";
 
-$current_page = (int) get_query_var( 'paged' );
-$count = ( $current_page > 1 ) ? ( ( $current_page - 1) * (int) get_option( 'posts_per_page' ) ) : 0;
+$count = abbey_posts_count();
 
 $abbey_query = array();
 
@@ -16,11 +16,12 @@ $queried_name = $queried_object->slug;
 $archive_options = ( !empty( $abbey_defaults[ "archive" ] ) ) ? $abbey_defaults[ "archive" ] : array();
 ?>
 
-	<main id="<?php abbey_theme_page_id(); ?>" class="row archives">
+	<main id="<?php abbey_theme_page_id(); ?>" class="row archives <?php abbey_page_class(); ?>">
 		
 		<header id="<?php echo $queried_name; ?>-archive-header" class="text-center archive-header">
 			
 			<div class="md-50"><?php do_action( "abbey_archive_page_heading", $queried_object ); ?></div>
+
 			<?php print_r( get_term_meta( $queried_object->term_id, "thumbnail", true ) ); ?>
 
 		</header>
@@ -28,10 +29,16 @@ $archive_options = ( !empty( $abbey_defaults[ "archive" ] ) ) ? $abbey_defaults[
 		<section id="content" class="row archive-content">
 
 			<?php if ( have_posts() ) : abbey_setup_query(); ?>
+
+				<header class="archive-post-slides posts-slides" 
+					data-slick='{"slidesToShow":1, "slidesToScroll":1,"autoplay":false,"arrows" : false, "rtl":true, "dots": true, "infinite":true}'
+				>
+					<?php do_action( "abbey_theme_archive_slide", $wp_query->posts ); ?> 
+				</header>
 				
 				<?php if( !empty( (bool) $archive_options[ "sidebar" ] ) ) : ?>
 					<div class="col-md-3 archive-summary" id="<?php echo $queried_name; ?>-archive-summary">
-					<?php do_action( "abbey_archive_page_summary", $abbey_query ); ?>
+						<?php do_action( "abbey_archive_page_summary", $abbey_query ); ?>
 					</div>
 				<?php endif; ?>
 
@@ -48,6 +55,7 @@ $archive_options = ( !empty( $abbey_defaults[ "archive" ] ) ) ? $abbey_defaults[
 							<?php abbey_posts_pagination();?>
 						<?php endif; ?>
 					</div>
+
 				</div>
 
 		
@@ -58,6 +66,6 @@ $archive_options = ( !empty( $abbey_defaults[ "archive" ] ) ) ? $abbey_defaults[
 		</section>
 		
 
-	</main>		<div style="direction: ltr;"> <?php print_r( (bool) $archive_options[ "sidebar" ] ); ?></div><?php
+	</main>		<div style="direction: ltr;"> <?php //print_r(); ?></div><?php
 
 get_footer();
