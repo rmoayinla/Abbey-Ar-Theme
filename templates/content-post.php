592f51d4cd9205bbb56a87cb312c378ba022be8f
rmoayinla <rmoayinla@yahoo.com>
2017-08-07 10:59:23 +0100

    Re-arranged the markup (again!), added a '.inner-wrap" to the ".post-entry" element to remove inner-margin, place the post-thumbnail and post-excerpt outside the ".post-entry" element

--- templates/content-post.php
+++ templates/content-post.php
@@ -13,27 +13,23 @@
 			<h1 class="post-title" itemprop="headline"><?php the_title(); ?></h1>
 		</header><!-- #page-content-header closes -->
 
-		<section class="post-entry row">
-				
-			<figure class="post-thumbnail" itemprop="image"><?php abbey_page_media( "large" ); ?> </figure>
-			<figcaption class="post-thumbnail-caption"><?php if( $has_thumbnail ) the_post_thumbnail_caption(); ?></figcaption>
+		<figure class="post-thumbnail" itemprop="image"><?php abbey_page_media( "large" ); ?> </figure>
+		<figcaption class="post-thumbnail-caption"><?php if( $has_thumbnail ) the_post_thumbnail_caption(); ?></figcaption>
+		
+		<summary class="post-excerpt"><?php the_excerpt(); ?></summary>
+		
+		<section class="post-entry row inner-wrap">	
 			
-
-			<article <?php abbey_post_class(); ?> id="post-<?php the_ID(); ?>">
-				
-				<summary class="post-excerpt"><?php the_excerpt(); ?></summary>
+			<article <?php abbey_post_class(); ?> id="post-<?php the_ID(); ?>">			
 				<?php the_content(); ?>
 				<div><?php abbey_post_pagination(); ?> </div>
-
 			</article>
 
 			<footer class="post-entry-footer"> <?php do_action( "abbey_theme_post_entry_footer", $title ); ?></footer>
 		
 		</section><!-- .post-entry closes -->
 
-		<footer class="entry-footer">
-			<?php if ( comments_open() ) comments_template(); ?>
-		</footer>
+		<footer class="entry-footer"><?php if ( comments_open() ) comments_template(); ?></footer>
 
 	</div>
 	
