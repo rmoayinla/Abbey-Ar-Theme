cae19e3ec4ebd4ce5b63eb2c784f56ea2e7f9733
rmoayinla <rmoayinla@yahoo.com>
2017-08-07 09:58:46 +0100

    Changed the markup of the page template to match and rhyme with that of post template, all the markup for the template is wrapped into a '.single-post-panel' div, having a heading '.entry-header', a body '.post-entry' and a footer '.entry-footer;

--- templates/content-page.php
+++ templates/content-page.php
@@ -3,13 +3,25 @@
 ?>
 <section  id="content" class="page-content col-md-6 col-md-offset-2">
 
-	<div id="page-media"><?php abbey_page_media( "large" ); ?> </div>
-			
-	<div class="row"><?php do_action("abbey_theme_before_page_content"); ?></div>
-	
-	<article <?php abbey_post_class(); ?> id="page-<?php the_ID(); ?>">
-		<?php the_content("Read more . . "); ?>
-	</article>
+	<div class="single-post-panel">
+		
+		<div class="row entry-header"><?php do_action("abbey_theme_before_page_content"); ?></div>
+
+		<section class="post-entry" id="page-entry">
+
+			<div id="page-media" class="post-thumbnail"><?php abbey_page_media( "large" ); ?> </div>
+		
+			<article <?php abbey_post_class(); ?> id="page-<?php the_ID(); ?>">
+				<?php the_content("Read more . . "); ?>
+			</article>
+
+		</section>
+
+		<footer class="entry-footer">
+
+		</footer>
+
+	</div>
 	
 	
 
